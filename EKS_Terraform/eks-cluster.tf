de115d8bcee7f36fc07e9ec0705083afa030452c
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-01-21 13:11:05 -0600

    Update eks-cluster.tf

--- EKS_Terraform/eks-cluster.tf
+++ EKS_Terraform/eks-cluster.tf
@@ -41,6 +41,7 @@ resource "aws_eks_cluster" "example" {
       "subnet-3cec9f63",
       "subnet-4adfa26b",
       "subnet-39bd9537",
+   ]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
