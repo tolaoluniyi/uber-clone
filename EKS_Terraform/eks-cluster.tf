5c1d1a9d5c043d12bf833a9bc187aa282192c5ba
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2024-01-21 12:20:57 -0600

    Update eks-cluster.tf

--- EKS_Terraform/eks-cluster.tf
+++ EKS_Terraform/eks-cluster.tf
@@ -33,7 +33,9 @@ data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
-    availability_zone = data.aws_availability_zones.available.names([0,1,2])
+    availability_zone = data.aws_availability_zones.available.names[0]
+    availability_zone = data.aws_availability_zones.available.names[1]
+    availability_zone = data.aws_availability_zones.available.names[2]
   }
 }
 #cluster provision
