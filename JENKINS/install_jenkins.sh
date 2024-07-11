cc1164e3bdb05c3f679353ea2b95bfc9ea85cb90
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-07-10 21:47:02 -0400

    jenkins file commit

--- JENKINS/install_jenkins.sh
+++ JENKINS/install_jenkins.sh
@@ -1,5 +1,5 @@
-sudo apt update
-sudo apt install openjdk-11-jdk -y
+sudo apt update -y
+sudo apt install openjdk-17-jre-headless -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
